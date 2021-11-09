cc92896bf6501418e285d5e3a58b5f9eaaca54cd
Matheus Reis <math.reis.dev@gmail.com>
2021-11-09 19:37:56 -0300

    Add files via upload

---
+++ InAndOut/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using InAndOut.Migrations;
+using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InAndOut.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Amount must be greater than zero!")]
+        public int Amount { get; set; }
+    }
+}
